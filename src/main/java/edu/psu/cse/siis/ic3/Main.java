0d2048b3d310a59f1eb414a6c0cd843db146cb19
Amiangshu Bosu <amiangshu.bosu@siu.edu>
2016-11-23 00:29:00 -0600

    Added code to check if app already analyzed

--- src/main/java/edu/psu/cse/siis/ic3/Main.java
+++ src/main/java/edu/psu/cse/siis/ic3/Main.java
@@ -1,12 +1,18 @@
 package edu.psu.cse.siis.ic3;
 
+
+import java.sql.SQLException;
+
 import edu.psu.cse.siis.ic3.db.SQLConnection;
+import edu.psu.cse.siis.ic3.manifest.ManifestPullParser;
 
 public class Main {
   public static void main(String[] args) {
 
     edu.psu.cse.siis.coal.Main.reset();
     SQLConnection.reset();
+    
+
 
     Ic3CommandLineParser parser = new Ic3CommandLineParser();
     Ic3CommandLineArguments commandLineArguments =
@@ -15,8 +21,25 @@ public class Main {
       return;
     }
     commandLineArguments.processCommandLineArguments();
+    
+    SQLConnection.init(commandLineArguments.getDbName(), "./cc.properties", null, 3306);
+    
+    ManifestPullParser manifestParser = new ManifestPullParser();
+	manifestParser.loadManifestFile(commandLineArguments.getInput());
+
+	try {
+		if (SQLConnection.checkIfAppAnalyzed(manifestParser.getPackageName(), manifestParser.getVersion())) {		
+			return;
+		}
+	
+    
     Ic3Analysis analysis = new Ic3Analysis(commandLineArguments);
     analysis.performAnalysis(commandLineArguments);
+    
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
   }
 
